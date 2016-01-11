06e53ce5072b50f0ee2f5e0557789d0405a66385
g <gradius040728@gmail.com>
2016-01-11 16:27:23 +0900

    First commit

---
+++ Assets/Script/TestScript01.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript01 : MonoBehaviour {
+
+
+	// feature20160110_01を実装。
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
