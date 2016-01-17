c0d670f436e065afb183045e4477cff1b6d5a821
nanko from mac <nanko0825@gmail.com>
2016-01-17 23:19:02 +0900

    mac : effectmanagerログ追加

--- Assets/Script/EffectManager.cs
+++ Assets/Script/EffectManager.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EffectManager : MonoBehaviour {
+public class EffectRef
+{
+	public ParticleSystem m_cParticle = null;
+	public bool m_bUse = false;
+}
+
+
+
+public class EffectManager : MonoBehaviour 
+{
+
+	private EffectRef[] m_acEffectRef = new EffectRef[ 64 ];
+	private int m_nLogOutputInterval = 0;
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +23,9 @@ public class EffectManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (--m_nLogOutputInterval < 0) {
+			m_nLogOutputInterval = 30;
+			Debug.LogWarning ( "EffectManager Update" );
+		}
 	}
 }
